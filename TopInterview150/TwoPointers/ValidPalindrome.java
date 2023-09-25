6f4d3c77f966ffcdd770d7d964951aa005063fc1
FelpGV <felpgv@gmail.com>
2023-09-24 23:39:34 -0500

    Valid Palindrome

---
+++ TopInterview150/TwoPointers/ValidPalindrome.java
@@ -0,0 +1,30 @@
+package TopInterview150.TwoPointers;
+//125. Valid Palindrome
+public class ValidPalindrome {
+    public static boolean isPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        boolean isPalindrome = true;
+        int i = 0;
+        int j = s.length()-1;
+        while(i<j){
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return isPalindrome;
+    }
+
+    public boolean isPalindrome2(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        StringBuilder reverse = new StringBuilder(s);
+        return s.contentEquals(reverse.reverse());
+    }
+
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        boolean result = isPalindrome(s);
+        System.out.println(result);
+    }
+}
