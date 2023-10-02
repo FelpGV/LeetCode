0f703f8c562c7452f19b4bc7dbb16346b968c3c9
FelpGV <felpgv@gmail.com>
2023-10-01 20:31:28 -0500

    Greatest Common Divisor of Strings

--- LeetCode75/ArrayString/GreatestCommonDivisorStrings.java
+++ LeetCode75/ArrayString/GreatestCommonDivisorStrings.java
@@ -1,15 +1,27 @@
 package LeetCode75.ArrayString;
 
+// 1071. Greatest Common Divisor of Strings
+
 public class GreatestCommonDivisorStrings {
+    public static int gcd(int maxStr, int minStr){
+        return (minStr == 0) ? maxStr : gcd(minStr, maxStr%minStr);
+    }
     public static String gcdOfStrings(String str1, String str2) {
-        String s = str1.replace(str2,"");
-        System.out.println(s);
-        return "";
+        if(!(str1+str2).equals(str2+str1)) return "";
+        int maxStr = Math.max(str1.length(), str2.length());
+        int minStr = Math.min(str1.length(), str2.length());
+        return str1.substring(0,gcd(maxStr,minStr));
     }
 
     public static void main(String[] args) {
-        String str1 = "ABCABC";
-        String str2 = "ABC";
+//        String str1 = "TAUXXTAUXXTAUXXTAUXXTAUXX";
+//        String str2 = "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX";
+//        String str1 = "ABABABAB";
+//        String str2 = "ABAB";
+//        String str1 = "CXTXNCXTXNCXTXNCXTXNCXTXN";
+//        String str2 = "CXTXNCXTXNCXTXNCXTXNCXTXNCXTXNCXTXNCXTXNCXTXNCXTXNCXTXNCXTXNCXTXN";
+        String str1 = "NLZGMNLZGMNLZGMNLZGMNLZGMNLZGMNLZGMNLZGM";
+        String str2 = "NLZGMNLZGMNLZGMNLZGMNLZGMNLZGMNLZGMNLZGMNLZGM";
         String result = gcdOfStrings(str1,str2);
         System.out.println(result);
     }
