367c08a12d1f4a351e83fe687063ca957bc41c1e
FelpGV <felpgv@gmail.com>
2023-10-12 22:30:34 -0500

    To Lower Case

---
+++ Problems_701_to_750/ToLowerCase.java
@@ -0,0 +1,13 @@
+package Problems_701_to_750;
+//709. To Lower Case
+public class ToLowerCase {
+    public static String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+
+    public static void main(String[] args) {
+        String s = "Hello";
+        String result =toLowerCase(s);
+        System.out.println(result);
+    }
+}
