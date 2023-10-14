fb224254f8c7b1a39ea4420512687eb8cf35e8c5
FelpGV <felpgv@gmail.com>
2023-10-13 23:28:59 -0500

    Intersection of Two Arrays

---
+++ Problems_1_to_50/Sum3.java
@@ -0,0 +1,15 @@
+package Problems_1_to_50;
+//3Sum
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sum3 {
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> n = new ArrayList<>();
+
+
+        return n;
+    }
+
+}
---
+++ Problems_301_to_350/IntersectionOfTwoArrays.java
@@ -0,0 +1,34 @@
+package Problems_301_to_350;
+//349. Intersection of Two Arrays
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class IntersectionOfTwoArrays {
+    public static int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> u = new HashSet<>();
+        ArrayList<Integer> n1 = new ArrayList<>();
+        for (int n : nums1) {
+            n1.add(n);
+        }
+        for (int n : nums2){
+            if(n1.contains(n)){
+                u.add(n);
+            }
+        }
+        int[] r = new int[u.size()];
+        int i = 0;
+        for (int n: u){
+            r[i++] = n;
+        }
+        return r;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {4,9,5};
+        int[] nums2 = {9,4,9,8,4};
+        int[] result = intersection(nums1,nums2);
+        System.out.println(Arrays.toString(result));
+    }
+}
